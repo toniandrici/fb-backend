e815867c6b2191de97cec557d134c82f6e03aa27
toniandrici <31852457+toniandrici@users.noreply.github.com>
2024-07-11 16:24:29 +0300

    f commit

---
+++ src/shared/globals/helpers/error-handler.ts
@@ -0,0 +1,88 @@
+import HTTP_STATUS from 'http-status-codes';
+
+export interface IErrorResponse {
+  message: string;
+  statusCode: number;
+  status: string;
+  serializeErrors(): IError;
+}
+
+export interface IError {
+  message: string;
+  statusCode: number;
+  status: string;
+}
+
+//Abstract inseamna a base class for us
+//abstract din clasa inseamna orice clasa abstracta care va extinde clasa de mai jos va trebui sa defineasca neaparat acele abstract properties
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+  abstract status: string;
+
+  constructor(message: string) {
+    super(message);
+  }
+
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      status: this.status,
+      statusCode: this.statusCode,
+    };
+  }
+}
+
+export class BadRequestError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  statusCode = HTTP_STATUS.NOT_FOUND;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = HTTP_STATUS.UNAUTHORIZED;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class FileToLargeError extends CustomError {
+  statusCode = HTTP_STATUS.REQUEST_TOO_LONG;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ServerError extends CustomError {
+  statusCode = HTTP_STATUS.SERVICE_UNAVAILABLE;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class JoiRequestValidationError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
