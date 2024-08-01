2f26cc7ea01284b065e77bd8551ab3ab61b47769
toniandrici <31852457+toniandrici@users.noreply.github.com>
2024-08-01 16:52:40 +0300

    few changes

--- src/features/auth/controllers/test/signout.test.ts
+++ src/features/auth/controllers/test/signout.test.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { authMockRequest, authMockResponse } from '@root/mocks/auth.mock';
-import { SignOut } from '@auth/controllers/signout';
+import { Signout } from '@auth/controllers/signout';
 
 const USERNAME = 'Manny';
 const PASSWORD = 'manny1';
@@ -9,14 +9,14 @@ describe('SignOut', () => {
   it('should set session to null', async () => {
     const req: Request = authMockRequest({}, { username: USERNAME, password: PASSWORD }) as Request;
     const res: Response = authMockResponse();
-    await SignOut.prototype.update(req, res);
+    await Signout.prototype.update(req, res);
     expect(req.session).toBeNull();
   });
 
   it('should send correct json response', async () => {
     const req: Request = authMockRequest({}, { username: USERNAME, password: PASSWORD }) as Request;
     const res: Response = authMockResponse();
-    await SignOut.prototype.update(req, res);
+    await Signout.prototype.update(req, res);
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({
       message: 'Logout successful',
