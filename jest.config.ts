04f8c493dbe5592c77fb85cef3d69a00508af63e
toniandrici <31852457+toniandrici@users.noreply.github.com>
2024-10-17 15:38:19 +0300

    feat: implemented post reactions

--- jest.config.ts
+++ jest.config.ts
@@ -29,6 +29,7 @@ const config: Config = {
     '@auth/(.*)': ['<rootDir>/src/features/auth/$1'],
     '@user/(.*)': ['<rootDir>/src/features/user/$1'],
     '@post/(.*)': ['<rootDir>/src/features/post/$1'],
+    '@reaction/(.*)': ['<rootDir>/src/features/reactions/$1'],
     '@global/(.*)': ['<rootDir>/src/shared/global/$1'],
     '@service/(.*)': ['<rootDir>/src/shared/services/$1'],
     '@socket/(.*)': ['<rootDir>/src/shared/sockets/$1'],
